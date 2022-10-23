349759c43e0b07bae1e8207200e7cd02d0aacc96
JakobLarsson <jakob.larsson@live.se>
2022-10-23 19:03:24 +0200

    added single-page-project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { SinglePageProjectComponent } from './components/single-page-project/single-page-project.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    path: 'singleproject',
+    component: SinglePageProjectComponent,
   },
 ];
 
